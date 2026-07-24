aa2d59c69ea8238e200ce5fcb051665fbaf45f65
Elena Poelman <elena.poelman@redpencil.io>
2026-07-24 11:06:29 +0200

    ldes-delta-pusher: set-up checkpoint creation

--- config/ldes-delta-pusher/initialization.ts
+++ config/ldes-delta-pusher/initialization.ts
@@ -2,6 +2,14 @@ import { RESOURCE_TYPES } from "./constants";
 
 export const initialization = {
   "ipdc-enriched": Object.fromEntries(
-    RESOURCE_TYPES.map((type) => [type, {}])
+    RESOURCE_TYPES.map((type) => [
+      type,
+      {
+        graphFilter: `VALUES ?g {
+                        <http://mu.semte.ch/graphs/ipdc/ldes-data>
+                        <http://mu.semte.ch/graphs/ipdc/enrichments>
+                      }`,
+      },
+    ]),
   ),
 };
